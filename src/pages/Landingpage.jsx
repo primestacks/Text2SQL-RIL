285345f1b65dc3e44624c318183972c6dd46a30f
Primedev100 <tonystack424@gmail.com>
2025-02-28 07:48:02 +0100

    fix component directory mismatch

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,10 +1,10 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import HeroHome from "../components/Herohome";
 import OurGrowth from "../components/OurGrowth";
 import FAQ from "../components/FAQ";
 import Testimonial from "../components/Testimonial";
 import HowItWorks from "../components/HowItWorks";
+import HeroHome from "../components/Herohome";
 
 function Landingpage() {
   return (
