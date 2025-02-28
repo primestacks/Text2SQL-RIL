35a5e46ecce46dda177f77b09f76ef7bb52c13ec
Primedev100 <tonystack424@gmail.com>
2025-02-28 10:30:40 +0100

    update componenet naming on landing page

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -4,13 +4,13 @@ import OurGrowth from "../components/OurGrowth";
 import FAQ from "../components/FAQ";
 import Testimonial from "../components/Testimonial";
 import HowItWorks from "../components/HowItWorks";
-import Herohome from "../components/Herohome";
+import Hero_home from "../components/Hero_home";
 
 function Landingpage() {
   return (
     <div>
       <Navbar />
-      <Herohome />
+      <Hero_home />
       <OurGrowth />
       <HowItWorks />
       <FAQ />
